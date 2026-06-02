cc3e18851e68e867db49c1fd7cd7dd5de62a829e
Dvii-www <grdavid950@gmail.com>
2026-06-01 18:34:42 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ✏️ EDITABLE: cambia este número cada vez que subas una actualización
 // Esto le avisa a todos los usuarios que hay versión nueva
-const VERSION = 'tapcolor-v12';
+const VERSION = 'tapcolor-v13';
 
 const ASSETS = ['./', './index.html'];
 
